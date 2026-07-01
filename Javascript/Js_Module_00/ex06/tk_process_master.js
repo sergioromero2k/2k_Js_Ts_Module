19fc77224452be960bc614d91ed6b8ffeb39eddf
Sergio Alejandro <sergioromero2k@gmail.com>
2026-07-01 15:18:54 +0200

    finished module 00

--- Javascript/Js_Module_00/ex06/tk_process_master.js
+++ Javascript/Js_Module_00/ex06/tk_process_master.js
@@ -1,5 +1,18 @@
 const convertidorTipo = require("./tk_type_converter2");
 
+/**
+ *
+ */
+function sanitizar (value) {
+  if (value == "true") {return true;}
+  if( value == "false") {return false;}
+
+  const num = Number(value);
+  if (!Number.isNaN(num)) {return num;}
+
+  return value;
+}
+
 console.log("============================================");
 console.log("PROCESS MASTER - Analizador argv      ");
 console.log("============================================");
@@ -8,7 +21,8 @@ if ((process.argv).length > 2) {
   console.log("Argumentos recibidos: ", (process.argv).length-2);
   console.log("");
   for(let i = 2; i < (process.argv).length; i++ ){
-    console.log("[",i,"] Valor: ",process.argv[i], "  | Tipo detectado: ", convertidorTipo(process.argv[i]));
+    const valorConvertido = sanitizar(process.argv[i]);
+    console.log("[",i-2,"] Valor: ",valorConvertido, "  | Tipo detectado: ", convertidorTipo(valorConvertido));
   };
 } else {
   console.log(" Uso: node tk_process_master.js <arg1> <arg2> ...");
